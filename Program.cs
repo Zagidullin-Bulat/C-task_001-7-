b577545929405cd68d237f1125e70c59978c7a15
Zagidullin-Bulat <120476991+Zagidullin-Bulat@users.noreply.github.com>
2023-01-10 17:01:50 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// Тип данных для элементов выбрать double, наличие ненулевой дробной части обязательно.
+
+double[,] numbers = new double[3, 4];
+FillArray(numbers);
+PrintArray(numbers);
+
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray (double [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
